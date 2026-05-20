d7fc09b6628f6b0b9d72026ce6f7946f76428cb8
UnladenCoconut <benjkollar03@gmail.com>
2026-05-20 22:00:47 +0100

    allow using a GUID type directly when declaring an interface

--- crates/libs/interface/src/guid.rs
+++ crates/libs/interface/src/guid.rs
@@ -1,21 +1,32 @@
 //! GUID parsing and token generation for the `#[interface]` macro attribute.
 
-use quote::quote;
+use quote::{ToTokens, quote};
 
 /// The parsed GUID attribute on an `#[interface]` invocation.
 ///
 /// ```rust,ignore
 /// #[windows_interface::interface("8CEEB155-2849-4ce5-9448-91FF70E1E4D9")]
 ///                              //^ this part is parsed as a Guid
+/// Or can pass a GUID value directly as an expression:
+/// #[windows_interface::interface(GUID {
+//    data1: 0x8ceeb155,
+//    data2: 0x2849,
+//    data3: 0x4ce5,
+//    data4: [0x94, 0x48, 0x91, 0xff, 0x70, 0xe1, 0xe4, 0xd9],
+//})]
 /// unsafe trait IUIAnimationVariable: IUnknown { ... }
 /// ```
 ///
 /// When the attribute is absent (i.e. `#[interface]` with no argument), the GUID is `None`
 /// and `to_tokens` emits `GUID::zeroed()`.
-pub(crate) struct Guid(pub(crate) Option<syn::LitStr>);
+pub(crate) enum Guid {
+    None,
+    LitStr(syn::LitStr),
+    Expr(syn::Expr),
+}
 
 impl Guid {
-    /// Converts the parsed GUID string into a `::windows_core::GUID { ... }` token stream.
+    /// Converts the parsed GUID string or expression into a `::windows_core::GUID { ... }` token stream.
     pub(crate) fn to_tokens(&self) -> syn::Result<proc_macro2::TokenStream> {
         fn hex_lit(num: &str) -> syn::LitInt {
             syn::LitInt::new(&format!("0x{num}"), proc_macro2::Span::call_site())
@@ -49,53 +60,60 @@ impl Guid {
             Ok(part.to_owned())
         }
 
-        if let Some(value) = &self.0 {
-            let guid_value = value.value();
-            let mut delimited = guid_value.split('-').fuse();
-            let chunks = [
-                ensure_length(delimited.next(), 0, 8, value.span())?,
-                ensure_length(delimited.next(), 1, 4, value.span())?,
-                ensure_length(delimited.next(), 2, 4, value.span())?,
-                ensure_length(delimited.next(), 3, 4, value.span())?,
-                ensure_length(delimited.next(), 4, 12, value.span())?,
-            ];
-
-            let data1 = hex_lit(&chunks[0]);
-            let data2 = hex_lit(&chunks[1]);
-            let data3 = hex_lit(&chunks[2]);
-            let (data4_1, data4_2) = chunks[3].split_at(2);
-            let data4_1 = hex_lit(data4_1);
-            let data4_2 = hex_lit(data4_2);
-            let (data4_3, rest) = chunks[4].split_at(2);
-            let data4_3 = hex_lit(data4_3);
-            let (data4_4, rest) = rest.split_at(2);
-            let data4_4 = hex_lit(data4_4);
-            let (data4_5, rest) = rest.split_at(2);
-            let data4_5 = hex_lit(data4_5);
-            let (data4_6, rest) = rest.split_at(2);
-            let data4_6 = hex_lit(data4_6);
-            let (data4_7, data4_8) = rest.split_at(2);
-            let data4_7 = hex_lit(data4_7);
-            let data4_8 = hex_lit(data4_8);
-            Ok(quote! {
-                ::windows_core::GUID {
-                    data1: #data1,
-                    data2: #data2,
-                    data3: #data3,
-                    data4: [#data4_1, #data4_2, #data4_3, #data4_4, #data4_5, #data4_6, #data4_7, #data4_8]
-                }
-            })
-        } else {
-            Ok(quote! {
+        match self {
+            Self::None => Ok(quote! {
                 ::windows_core::GUID::zeroed()
-            })
+            }),
+            Self::Expr(expr) => Ok(expr.to_token_stream()),
+            Self::LitStr(value) => {
+                let guid_value = value.value();
+                let mut delimited = guid_value.split('-').fuse();
+                let chunks = [
+                    ensure_length(delimited.next(), 0, 8, value.span())?,
+                    ensure_length(delimited.next(), 1, 4, value.span())?,
+                    ensure_length(delimited.next(), 2, 4, value.span())?,
+                    ensure_length(delimited.next(), 3, 4, value.span())?,
+                    ensure_length(delimited.next(), 4, 12, value.span())?,
+                ];
+
+                let data1 = hex_lit(&chunks[0]);
+                let data2 = hex_lit(&chunks[1]);
+                let data3 = hex_lit(&chunks[2]);
+                let (data4_1, data4_2) = chunks[3].split_at(2);
+                let data4_1 = hex_lit(data4_1);
+                let data4_2 = hex_lit(data4_2);
+                let (data4_3, rest) = chunks[4].split_at(2);
+                let data4_3 = hex_lit(data4_3);
+                let (data4_4, rest) = rest.split_at(2);
+                let data4_4 = hex_lit(data4_4);
+                let (data4_5, rest) = rest.split_at(2);
+                let data4_5 = hex_lit(data4_5);
+                let (data4_6, rest) = rest.split_at(2);
+                let data4_6 = hex_lit(data4_6);
+                let (data4_7, data4_8) = rest.split_at(2);
+                let data4_7 = hex_lit(data4_7);
+                let data4_8 = hex_lit(data4_8);
+                Ok(quote! {
+                    ::windows_core::GUID {
+                        data1: #data1,
+                        data2: #data2,
+                        data3: #data3,
+                        data4: [#data4_1, #data4_2, #data4_3, #data4_4, #data4_5, #data4_6, #data4_7, #data4_8]
+                    }
+                })
+            }
         }
     }
 }
 
 impl syn::parse::Parse for Guid {
     fn parse(cursor: syn::parse::ParseStream) -> syn::Result<Self> {
-        let string: Option<syn::LitStr> = cursor.parse().ok();
-        Ok(Self(string))
+        if let Ok(string) = cursor.parse::<syn::LitStr>() {
+            Ok(Self::LitStr(string))
+        } else if let Ok(expr) = cursor.parse::<syn::Expr>() {
+            Ok(Self::Expr(expr))
+        } else {
+            Ok(Self::None)
+        }
     }
 }
